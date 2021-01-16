63643c6de1e4a751b5f61cfd63ed0af46a8bc0de
Valentina Masalkova <vrassone@gmail.com>
2021-01-16 14:50:11 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    for (let bracket of str) {
+      let config = bracketsConfig.find(x => x.findIndex(y => y === bracket) !== -1);
+      if (!config) {
+        return false;
+      }
+      if (config[0] === config[1]) {
+        if (stack[stack.length - 1] === bracket) {
+          stack.pop();
+        } else {
+          stack.push(bracket);
+        }
+      } else {
+        if (config[0] === bracket) {
+          stack.push(bracket);
+        } else {
+          if (stack[stack.length - 1] !== config[0]) {
+            return false;
+          } else {
+            stack.pop();
+          }
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+
